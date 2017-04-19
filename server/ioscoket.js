e48c4e13f48279b3924a6c62983f49ec056c80c4
bondarevk <kirill300300@gmail.com>
2017-04-19 12:31:18 +0300

    commands

--- server/ioscoket.js
+++ server/ioscoket.js
@@ -1,4 +1,5 @@
 const shortid = require('shortid');
+const dns = require('dns');
 
 class Message {
   constructor(text, sender, timestamp) {
@@ -20,17 +21,96 @@ class Message {
 
 class IOSocket {
 
+  static checkPermissionLevel(socket, level) {
+    if (socket.ulevel) {
+      return socket.ulevel >= level;
+
+    }
+  }
+
   static init(io) {
+    this.io = io;
+    this.commands = {
+      ['/username']: (socket, body) => {
+        if (body) {
+          this.say(socket.username + ' изменил имя на "' + body + '"');
+          socket.username = body;
+        }
+      },
+      ['/mute']: (socket, body) => {
+        if (this.checkPermissionLevel(socket, 5)) {
+          if (body) {
+            this.say(socket.username + ' запретил писать в чат ' + body + '!');
+            this.mutes[body] = true;
+          }
+        }
+      },
+      ['/unmute']: (socket, body) => {
+        if (this.checkPermissionLevel(socket, 5)) {
+          if (body) {
+            this.say(socket.username + ' разрешил писать в чат ' + body + '!');
+            if (this.mutes.hasOwnProperty(body)) {
+              delete this.mutes[body];
+            }
+          }
+        }
+      }
+    };
+    this.mutes = {};
+
     this.messagesHistory = [];
 
     io.on('connection', (socket) => {
+
+      let ip = socket.client.conn.remoteAddress;
+      if (ip.length > 7 && ip.startsWith('::ffff:')) {
+        ip = ip.substring(7);
+      }
+      socket.ip = ip;
+      socket.username = ip;
+
+      if (ip === '::1') {
+        socket.ulevel = 5;
+      }
+
+      dns.reverse(ip, function (err, domains) {
+        if (err) {
+          return;
+        }
+
+        if (domains.length > 0) {
+          let host = domains[0].replace(/\s*\.itstep\.lan\s*/g, '');
+          if (socket.username === socket.ip) {
+            socket.username = host;
+          }
+        }
+      });
 
       socket.emit('clear');
       socket.emit('new', this.generateMessagesHistory());
 
       socket.on('create message', (message) => {
+        if (this.mutes.hasOwnProperty(socket.ip)) {
+          if (this.mutes[socket.ip] === true) {
+            return;
+          }
+        }
+
         if (message.text) {
-          let receivedMessage = new Message(message.text);
+          let messageParse = message.text.split(' ');
+          if (messageParse.length > 0) {
+            let command = messageParse[0].toLowerCase();
+            if (this.commands.hasOwnProperty(command)) {
+              let body = null;
+              if (messageParse.length > 1) {
+                body = message.text.substr(message.text.indexOf(' ') + 1);
+              }
+              this.commands[command](socket, body);
+              return;
+            }
+          }
+
+          let receivedMessage = new Message(message.text, socket.username + ' (' + socket.ip + ')');
           this.messagesHistory.push(receivedMessage);
           io.emit('new', receivedMessage.ToDataObject());
         }
@@ -40,6 +120,12 @@ class IOSocket {
         console.log('user disconnected');
       });
     });
+  }
+
+  static say(text) {
+    let newMessage = new Message(text, 'Server');
+    this.messagesHistory.push(newMessage);
+    this.io.emit('new', newMessage.ToDataObject());
   }
 
   static generateMessagesHistory() {
