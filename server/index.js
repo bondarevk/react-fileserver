f14dddc0de97d1148a81034ff26c4960d1b16df4
LockDownRU <kirill300300@gmail.com>
2017-04-22 15:56:53 +0300

    Change port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
